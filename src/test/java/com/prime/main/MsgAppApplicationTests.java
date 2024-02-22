68c70ae4d78f72e8fb47d66542734fcf34226f19
zac <zacwilliams11@hotmail.com>
2024-02-22 12:09:23 +1100

    Initial commit, functional web server with spring boot

---
+++ src/test/java/com/prime/main/MsgAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prime.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
